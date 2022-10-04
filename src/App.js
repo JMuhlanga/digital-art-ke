2990be8bfffaea68293f5eb3dfda76946af84215
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-10-04 21:47:34 +0300

    Addition of Key Pages and links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+import Home from './Components/Home';
+import Gallery from './Components/Gallery';
+import Events from './Components/Events';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Switch>
+
+        <Route exact path ="/home">
+          <Home />
+        </Route>
+
+        <Route path ="/gallery">
+          <Gallery />
+        </Route>
+
+        <Route path ="/events">
+          <Events />
+        </Route>
+
+        <Route path ="/about">
+          <About />
+        </Route>
+        
+        <Route path ="/contact">
+          <Contact />
+        </Route>
+        
+      </Switch>
+     
     </div>
   );
 }
---
+++ src/Components/Events.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Events() {
+  return (
+    <div className="Events">
+     
+    </div>
+  );
+}
+
+export default Events;
