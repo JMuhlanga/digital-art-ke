ff8385fcfb30cfdc11434bdc9bbb79e62e0ad344
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-10-05 23:44:10 +0300

    Updates

---
+++ precache-manifest.9695a518008a87ed479b54d4d9b6d069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "47f7a22011c4ab20ff7e",
+    "url": "/digital-art-ke/static/css/main.4b2335cf.chunk.css"
+  },
+  {
+    "revision": "47f7a22011c4ab20ff7e",
+    "url": "/digital-art-ke/static/js/main.47f7a220.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/digital-art-ke/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/digital-art-ke/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "2e084364295ddc7e1633",
+    "url": "/digital-art-ke/static/js/runtime~main.2e084364.js"
+  },
+  {
+    "revision": "cd3d2c78702e6334989cb4e18485a041",
+    "url": "/digital-art-ke/static/media/pexels-daian-gan-102127.cd3d2c78.jpg"
+  },
+  {
+    "revision": "249ae39fa9841e1c3fdf45131dc053f5",
+    "url": "/digital-art-ke/index.html"
+  }
+];
