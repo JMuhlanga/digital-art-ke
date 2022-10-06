14ade4b3dca48931a9781ba704005b51dcd0282e
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-10-06 10:39:27 +0300

    Update after deployment to Heroku

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,16 +9,16 @@ function Contact() {
 
                 <div className="info">
 
-                    <div>
+                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
                         <h3>EMAIL US</h3>
                         <p>info@digiartske.co.ke</p>
                     </div>
 
-                    <div>
+                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
                         <h3>CALL US</h3>
                         <p>(+254) 11-377-1641</p>
                     </div>
-                    <div>
+                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
                         <h3>FIND US</h3>
                         <p>Nairobi Kenya</p>
                     </div>
